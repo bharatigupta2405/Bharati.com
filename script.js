35db3232b2a012933f90a9daef9ff2921a045533
bharatigupta2405 <bharatigupta2405@gmail.com>
2023-09-20 18:36:27 +0530

    Modified According to Latest Data

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: [ "FrontEnd Developer", "UI Designer", "Blogger", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: [ "FrontEnd Developer", "UI Designer", "Blogger", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
